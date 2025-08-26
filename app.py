89611e2097f85cd9216a6beb2958b14e173f402d
omhela25-stack <yashhela25@gmail.com>
2025-08-26 20:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -497,10 +497,5 @@ models can be wrong. Not financial advice. Always perform independent due dilige
 </div>
 """, unsafe_allow_html=True)
 
-    comparison_lr_df = pd.DataFrame({
- "Actual": yte_lr_inv.flatten(),
- "Predicted": predicted_lr_inv.flatten()
-})
-st.line_chart(comparison_lr_df)
-st.write(f"Linear Regression Mean Squared Error: {lr_mse:.4f}")
+   
 
